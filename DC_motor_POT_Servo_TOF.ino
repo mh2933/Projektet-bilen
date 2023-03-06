2bc85a778fd5c6e4e6271d335361d1ea832bc2ce
mh2933 <42110442+mh2933@users.noreply.github.com>
2023-03-06 10:10:19 +0100

    Add files via upload

---
+++ DC_motor_POT_Servo_TOF.ino
@@ -0,0 +1,92 @@
+/*
+ Controlling a servo position using a potentiometer (variable resistor)
+ by Michal Rinott <http://people.interaction-ivrea.it/m.rinott>
+
+ modified on 8 Nov 2013
+ by Scott Fitzgerald
+ http://www.arduino.cc/en/Tutorial/Knob
+*/
+//This code is to control the speed of a DC motor by a potentiometer using l298n driver
+//We read the value from the analog input, calibrate it then inject to the module
+//Refer to Surtrtech youtube channel for more information
+int in1 = 10; //Declaring where our module is wired
+int in2 = 6;
+int ConA = 11;// Don't forget this is a PWM DI/DO
+int speed1;
+#define tof_sensor_read 2
+
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+
+int potpin = 1;  // analog pin used to connect the potentiometer
+int val;         // variable to read the value from the analog pin
+
+
+void setup() {
+  Serial.begin(9600);  
+
+  pinMode(6, OUTPUT);
+  pinMode(10, OUTPUT);  
+  pinMode(11, OUTPUT);
+
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+}
+
+void TurnMotorA(){ //We create a function which control the direction and speed
+  digitalWrite(in1, LOW); //Switch between this HIGH and LOW to change direction
+  digitalWrite(in2, HIGH);
+  speed1 = analogRead(A0);
+  speed1 = speed1*0.2492668622; //We read thea analog value from the potentiometer calibrate it
+  analogWrite(ConA,speed1);// Then inject it to our motor
+}
+
+/*void tof_sensor(void)
+{
+  uint16_t sensor_val = pulseIn(tof_sensor, HIGH);
+  Serial.print(sensor_val);
+ if (sensor_val < 1200)
+{
+  myservo.write(180);
+}   
+ else if (sensor_val > 1200 && sensor_val < 1900)
+{
+  myservo.write(0);
+} 
+ else
+ {
+ myservo.write(90);
+ }
+  
+}*/
+
+/*void servo()
+{
+  val = analogRead(potpin);         // reads the value of the potentiometer (value between 0 and 1023)
+  val = map(val, 0, 1023, 0, 180);  // scale it to use it with the servo (value between 0 and 180)
+  myservo.write(val);               // sets the servo position according to the scaled value
+  delay(1);
+
+  Serial.println(val);
+  delay(1);  // waits for the servo to get there
+}*/
+
+void loop() {
+TurnMotorA(); //one function that keeps looping you can add another one with different direction or stop
+//servo();
+//tof_sensor();
+uint16_t sensor_val = pulseIn(tof_sensor_read, HIGH);
+Serial.println(sensor_val);
+
+/*if (sensor_val > 1900)
+{
+  myservo.write(90);
+}
+else if (sensor_val < 1200)
+{
+  myservo.write(180);
+}*/
+
+myservo.write(180);
+
+}
